3cf07036bda45961061eb424e08bfeb072798fa9
CodeGottes <jojo.spieler@gmx.de>
2026-06-11 18:17:51 +0200

    HP, Kollision mit Tags
    Speedboost auf Shift/HP up/ Tot --> wird erkannt wen mit entsprechendem Tag kollidiert wird

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] int Hitpoints = 3;
+    bool speedboost = false;
+    Vector3 startpoint;
+
     // Make isGrounded visible in the inspector for debugging
     [SerializeField] private bool isGrounded = true;
     // Distance for the ground check raycast
@@ -15,12 +19,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpHeight = 2f;
     [SerializeField] private float gravity = -9.81f;
 
-    //Camera parameters
-    [SerializeField] GameObject mainCam; 
-    [SerializeField] Transform cameraFollowTarget;
-    float xRotation;
-    float yRotation;
-    [SerializeField] float empfindlich = 0.2f;
 
     private float lastJumpTime;
     // A reference to the Animator component
@@ -29,19 +27,28 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     // Private variable for vertical velocity (gravity and jump)
     private float verticalVelocity;
+    
+    
+    //Camera parameters
+    [SerializeField] GameObject mainCam; 
+    [SerializeField] Transform cameraFollowTarget;
+    float xRotation;
+    float yRotation;
+    [SerializeField] float empfindlich = 0.2f;
 
     void Start()
     {
         // Get references to the Animator and CharacterController components
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        startpoint = transform.position;
     }
 
     void Update()
     {
         // Ground check based on raycast
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
-        Debug.Log(isGrounded);
+        //Debug.Log(isGrounded);
         // Draw the raycast to visualize it in the editor
         Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundCheckDistance), Color.red);
 
@@ -57,13 +64,23 @@ public class PlayerController : MonoBehaviour
         // Set the "isRunning" param in the animator
         animator.SetBool("isRunning", isMoving);
 
+        float sprintput = InputSystem.actions["Player/Sprint"].ReadValue<float>();
+        Debug.Log(sprintput);
         float targetRotation = 0;
         float speed = 0;
 
         // -- Movement & Rotation --
         if (isMoving)
-        {
+        {   
             speed = movementSpeed;
+
+            if (sprintput == 1)
+            {
+                if (speedboost)
+                {
+                    speed = speed * 4;
+                }
+            }
             // Calcualte the target rotation based on the movement direction
             targetRotation = Quaternion.LookRotation(moveDir).eulerAngles.y + mainCam.transform.rotation.eulerAngles.y;
             
@@ -101,6 +118,13 @@ public class PlayerController : MonoBehaviour
         controller.Move(finalMovement * Time.deltaTime);
 
         CameraRotation();
+
+        if (Hitpoints <= 0)
+        {
+            transform.position = startpoint;
+            Hitpoints = 1;
+        }
+
     }
 
     void CameraRotation()
@@ -112,5 +136,21 @@ public class PlayerController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation,-10,50);
         Quaternion camrotation = Quaternion.Euler(xRotation, yRotation, 0);
         cameraFollowTarget.rotation = camrotation;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "dead")
+        {
+            Hitpoints = 0;
+        }
+        if (hit.gameObject.tag == "hp")
+        {
+            Hitpoints++;
+        }
+        if(hit.gameObject.tag == "boost")
+        {
+            speedboost = true;
+        }
     }
 }
